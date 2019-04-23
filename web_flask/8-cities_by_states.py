ed9aeb87eb19bfc58e89d4bf1618aa127c3d511b
Martin Smith <msmith3dart@icloud.com>
2019-04-23 04:54:49 +0000

    add task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from web_flask import app
+from flask import render_template, abort
+from models import storage
+
+@app.route('/states_list')
+def list_state():
+    states = []
+    for key, state in storage.all("State").items():
+        states.append({
+            'id': state.id,
+            'name': state.name,
+            })
+    
+    return render_template("7-states_list.html", states=states)
+
+@app.route('/cities_by_state')
+def cities_by_state():
+    states = []
+    for key, state in storage.all("State").items():
+        states.append({
+            'id': state.id,
+            'name': state.name,
+            'cities': state.cities
+            })
+    
+    return render_template("8-cities_by_states.html", states=states)
+    
+@app.teardown_appcontext
+def teardown_storage(self):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run()
